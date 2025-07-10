07f2603b93a763418598a0a39b25c7de9e01baa0
Alina <alina.redchin@gmail.com>
2025-07-10 20:34:59 +0200

    add test_unsuccessful_password_reset

--- testcases/test_password_reset.py
+++ testcases/test_password_reset.py
@@ -8,3 +8,9 @@ class TestPasswordReset:
         password_reset_page.click_forgot_password_the_link()
         password_reset_page.reset_password("Admin")
         assert password_reset_page.password_reset_modal_is_displayed()
+
+    def test_unsuccessful_password_reset(self, driver):
+        password_reset_page = PasswordResetPage(driver)
+        password_reset_page.click_forgot_password_the_link()
+        password_reset_page.reset_password("")
+        assert password_reset_page.error_message_is_displayed
