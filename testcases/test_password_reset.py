94991f9c154194746a5a58c65110bb4cc050ad06
Alina <alina.redchin@gmail.com>
2025-07-10 21:00:41 +0200

    add test_cancel_password_reset

--- testcases/test_password_reset.py
+++ testcases/test_password_reset.py
@@ -1,3 +1,4 @@
+import urls
 from pages.PasswordResetPage import PasswordResetPage
 
 
@@ -14,3 +15,11 @@ class TestPasswordReset:
         password_reset_page.click_forgot_password_the_link()
         password_reset_page.reset_password("")
         assert password_reset_page.error_message_is_displayed
+
+    def test_cancel_password_reset(self, driver):
+        password_reset_page = PasswordResetPage(driver)
+        password_reset_page.click_forgot_password_the_link()
+        password_reset_page.cancel_password_reset()
+        current_page = password_reset_page.current_url
+        assert current_page == urls.BaseUrl
+
