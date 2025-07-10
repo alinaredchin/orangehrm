77fc241e0d9686a791f4aac18c5dbb255929186e
Alina <alina.redchin@gmail.com>
2025-07-10 14:16:15 +0200

    add headless mode

--- conftest.py
+++ conftest.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 
-# def pytest_addoption(parser):
-#    parser.addoption("--headless", action="store_true",
-#                      help="Run browser in headless mode")
+def pytest_addoption(parser):
+    parser.addoption("--headless", action="store_true",
+                     help="Run browser in headless mode")
 
 
 @pytest.hookimpl(tryfirst=True)
@@ -26,10 +28,15 @@ def pytest_html_report_title(report):
 
 @pytest.fixture
 def driver():
-    options = webdriver.ChromeOptions()
-    # options.add_argument("--headless=new")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    driver.maximize_window()
+    options = Options()
+    options.add_argument("--headless=new")  # Use headless mode in CI
+    options.add_argument("--no-sandbox")  # Needed in many CI environments
+    options.add_argument("--disable-dev-shm-usage")  # Prevent shared memory issues
+    options.add_argument("--window-size=1920,1080")
+
+    service = ChromeService(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    # driver.maximize_window()
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
